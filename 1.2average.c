b744b438a191b82c17a6d1287aab1a4ae3bf2646
wlin <linfromearth@outlook.com>
2016-06-02 15:38:16 +0800

    1.2average
    上次提交2.1应该是1.2 的

---
+++ 1.2average.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+	int a,b;
+	printf("please input first number and second number:");
+	scanf("%d %d",&a,&b);
+	double average;
+	printf("the average is %lf",average = (a+b)/2.0);
+	return 0;
+}
