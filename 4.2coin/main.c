2bef7a9ad33c99a49aec802dbd4b780c2cbe7db0
wlin <linfromearth@outlook.com>
2016-06-17 23:11:52 +0800

    4.2coin

---
+++ 4.2coin/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int one,two,five;
+    int x;
+    int exit = 0;
+    printf("请输入钱数:");
+    scanf("%d",&x);
+    for (one=1;one<=x*10;one++){
+        for (two=1;two<=x*10/2;two++){
+            for (five=1;five<=x*10/5;five++){
+                if (one+two*2+five*5 == x*10){
+                    printf("可以用%d个1角加%d个2角加%d个5角合成%d元\n",one,two,five,x);
+                    exit = 1;
+                    break;
+                }
+            }
+            if (exit == 1)break;
+        }
+        if (exit == 1)break;
+    }
+    return 0;
+}
