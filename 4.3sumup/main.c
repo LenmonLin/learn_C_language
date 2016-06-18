7c37eb70bca889c2fb9c10456f22cfed4aa5b239
wlin <linfromearth@outlook.com>
2016-06-18 11:26:17 +0800

    4.3 sum of reciprocals with 正负号相间

--- 4.3sumup/main.c
+++ 4.3sumup/main.c
@@ -5,10 +5,12 @@ int main()
 {
     int i,n;
     double sum = 0.0;
+    int sign = 1;
     printf("please input final number:");
     scanf("%d",&n);
     for(i=1;i<=n;i++){
-        sum += 1.0/i;
+        sum += 1.0*sign/i;
+        sign = -sign;
     }
     printf("the sum of reciprocals is :%lf",sum);
     return 0;
