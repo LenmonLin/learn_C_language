f3b3af9c619035b9b69cb1c34aa9ba952740d871
wlin <linfromearth@outlook.com>
2016-06-20 21:15:46 +0800

    4.3separate

---
+++ 4.3separate/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x;
+    printf("please input your number:");
+    scanf("%d",&x);
+    int mask=1;
+    int t = x;
+    while(t>9){
+        t /= 10;
+        mask *= 10;
+    }
+
+    do {
+        int d = x /mask;
+        printf("%d",d);
+        if (mask > 0){
+            printf(" ");
+        }
+        x %= mask;
+         mask /= 10;
+
+    }while( mask>0);
+    return 0;
+}
